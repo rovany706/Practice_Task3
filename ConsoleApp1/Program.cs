e3e1fb0cd077bdcc15a4c12eeae4a57420f1791b
Ivan <rovany706@gmail.com>
2018-04-20 15:36:56 +0500

    Added input and output of results

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -16,11 +16,25 @@ namespace ConsoleApp1
                 return Math.Pow(x, 2);
             else
                 return 4;
-
         }
         static void Main(string[] args)
         {
-            
+            Console.WriteLine("Задача 4\n=================");
+            Console.WriteLine("Условие задачи:\nДано действительное число a. Для функций f(x), " +
+                              "графики которых предствлены на рисунке, вычислить f(a).\n" +
+                              "=================");
+            double input;
+            bool ok;
+            Console.Write("Введите действительное число \"a\" для того, чтобы узнать значение функции f(a) на графике: ");
+            do
+            {
+                ok = double.TryParse(Console.ReadLine(), out input);
+                if (!ok)
+                    Console.WriteLine("Ошибка! Введите вещественное число.");
+            } while (!ok);
+
+            double result = GetValue(input);
+            Console.WriteLine("Ответ: Значение функции f(a) в точке a = {0} равно {1}", input, result);
         }
     }
 }
